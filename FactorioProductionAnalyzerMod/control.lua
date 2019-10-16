dba12bef0403dd9cb4b76b4a17108c4ca1296106
agrutter87 <agrutter87@gmail.com>
2019-10-15 22:16:40 -0500

    Updated mod scripts to use different JSON parser
    Incremented version
    Replacing the JSON parser corrects a problem where files saved with this mod enabled could no longer be opened.

--- FactorioProductionAnalyzerMod/control.lua
+++ FactorioProductionAnalyzerMod/control.lua
@@ -1,59 +1,80 @@
-require("json")
+local json = require "lib/json"
 
-local inputdata = {}
-local outputdata = {}
+local inputs = {}
+local outputs = {}
 
 function acquireStats()
 	-- Production Statistics
 	for _, player in pairs(game.players) do
-		i = 1
-		for name, value in pairs(player.force.item_production_statistics.input_counts) do
+		for item, input_count in pairs(player.force.item_production_statistics.input_counts) do
 			--player.print(item)
 			--player.print(input_count)
-			--inputs[name] = value
-			inputdata[i] = {["name"]=name, ["value"]=value}
-			i = i + 1
+			inputs[item] = input_count
 		end
 		
-		for name, value in pairs(player.force.fluid_production_statistics.input_counts) do
+		for fluid, input_count in pairs(player.force.fluid_production_statistics.input_counts) do
 			--player.print(fluid)
 			--player.print(input_count)
-			--inputs[fluid] = input_count
-			inputdata[i] = {["name"]=name, ["value"]=value}
-			i = i + 1
+			inputs[fluid] = input_count
 		end
+		--]]
 		
-		i = 1
-		for name, value in pairs(player.force.item_production_statistics.output_counts) do
+		---[[ Count outputs
+		for item, output_count in pairs(player.force.item_production_statistics.output_counts) do
 			--player.print(item)
 			--player.print(input_count)
-			--outputs[item] = output_count
-			outputdata[i] = {["name"]=name, ["value"]=value}
-			i = i + 1
+			outputs[item] = output_count
 		end
 		
 		for fluid, output_count in pairs(player.force.fluid_production_statistics.output_counts) do
 			--player.print(fluid)
 			--player.print(input_count)
-			--outputs[fluid] = output_count
-			outputdata[i] = {["name"]=name, ["value"]=value}
-			i = i + 1
+			outputs[fluid] = output_count
 		end
 	end
 end
 
-function updateJSONFile(timestamp)
+function processStats()
+	for thing, count in pairs(inputs) do
+		--player.print(thing)
+		--player.print(count)
+	end
 
-	productiondata = {["timestamp"]=timestamp, ["inputs"]=inputdata, ["outputs"]=outputdata}
+	for thing, count in pairs(outputs) do
+		--player.print(thing)
+		--player.print(count)
+	end
+end
+
+function updateJSONFile()
+
+	local inputdata = {}
+	local outputdata = {}
+
+	i = 1
+	for name, value in pairs(inputs) do
+		inputdata[i] = {["name"]=name, ["value"]=value}
+		i = i + 1
+	end
+	
+	i = 1
+	for name, value in pairs(outputs) do
+		outputdata[i] = {["name"]=name, ["value"]=value}
+		i = i + 1
+	end
+
+	productiondata = {["inputs"]=inputdata, ["outputs"]=outputdata}
 	
 	outdata = {["productiondata"]=productiondata}
 	
-	local outjson = global.json.stringify(outdata)
+  --game.players[1].print(global.json)
+	--local outjson = json:encode(outdata)
 	
-	local folder = "FactorioProductionAnalyzer/"
+	local folder = "FactorioProductionAnalzyer/"
 	local filename = "production_data.json"
 	
-	game.write_file(folder..filename, outjson, false)
+	--game.write_file(folder..filename, outjson, false)
+  game.write_file(folder..filename, json:encode(outdata), false)
 end
 
 script.on_event(defines.events.on_player_created, function(event)
@@ -64,7 +85,8 @@ script.on_event(defines.events.on_tick, function(event)
 	if event.tick % 60 == 0 then
 
 	acquireStats()
-	updateJSONFile(event.tick)
+	processStats()
+	updateJSONFile()
 
  	end
 end)
