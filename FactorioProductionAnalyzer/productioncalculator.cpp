904cdc5c7ad0e591c9898b8072a5230b5e56f847
agrutter87 <agrutter87@gmail.com>
2019-10-28 18:04:26 -0500

    Added functions to ProductionCalculator
    Added functions to ProductionCalculator Class:
    • fileRead
    • jsonRead
    
    Added members to hold QVectors for each Factorio class

--- FactorioProductionAnalyzer/productioncalculator.cpp
+++ FactorioProductionAnalyzer/productioncalculator.cpp
@@ -1,4 +1,6 @@
 #include "productioncalculator.h"
+#include <QFile>
+#include <QJsonDocument>
 
 /*************************************************************************
  * ProductionCalculator::ProductionCalculator
@@ -19,3 +21,103 @@ void ProductionCalculator::setFile(QString &fileName)
     mFileName = fileName;
 }
 
+/*************************************************************************
+ * ProductionAnalyzer::fileRead
+ *************************************************************************/
+bool ProductionCalculator::fileRead(void)
+{
+#if DEBUG_PRODUCTION_CALCULATOR
+    qDebug() << __PRETTY_FUNCTION__;
+#endif
+    QFile loadFile(mFileName);
+
+    if(!loadFile.open(QIODevice::ReadOnly))
+    {
+        qWarning("Couldn't open save file.");
+        return false;
+    }
+
+    QByteArray saveData = loadFile.readAll();
+
+    QJsonDocument loadDoc(QJsonDocument::fromJson(saveData));
+
+    jsonRead(loadDoc.object());
+
+    return true;
+}
+
+/*************************************************************************
+ * ProductionAnalyzer::jsonRead
+ *************************************************************************/
+void ProductionCalculator::jsonRead(const QJsonObject &json)
+{
+#if DEBUG_PRODUCTION_CALCULATOR
+    qDebug() << __PRETTY_FUNCTION__;
+#endif
+    if(json.contains("assemblers") && json["assemblers"].isObject())
+    {
+        QJsonObject object = json["assemblers"].toObject();
+        foreach (QString name, object.keys())
+        {
+            QJsonObject assembler = object[name].toObject();
+            FactorioAssembler newAssembler;
+            newAssembler.jsonRead(assembler);
+            mAssemblers.append(newAssembler);
+        }
+    }
+    if(json.contains("items") && json["items"].isObject())
+    {
+        QJsonObject object = json["items"].toObject();
+        foreach (QString name, object.keys())
+        {
+            QJsonObject item = object[name].toObject();
+            FactorioItem newItem;
+            newItem.jsonRead(item);
+            mItems.append(newItem);
+        }
+    }
+    if(json.contains("miners") && json["miners"].isObject())
+    {
+        QJsonObject object = json["miners"].toObject();
+        foreach (QString name, object.keys())
+        {
+            QJsonObject miner = object[name].toObject();
+            FactorioMiner newMiner;
+            newMiner.jsonRead(miner);
+            mMiners.append(newMiner);
+        }
+    }
+    if(json.contains("modules") && json["modules"].isObject())
+    {
+        QJsonObject object = json["modules"].toObject();
+        foreach (QString name, object.keys())
+        {
+            QJsonObject module = object[name].toObject();
+            FactorioModule newModule;
+            newModule.jsonRead(module);
+            mModules.append(newModule);
+        }
+    }
+    if(json.contains("recipes") && json["recipes"].isObject())
+    {
+        QJsonObject object = json["recipes"].toObject();
+        foreach (QString name, object.keys())
+        {
+            QJsonObject recipe = object[name].toObject();
+            FactorioRecipe newRecipe;
+            newRecipe.jsonRead(recipe);
+            mRecipes.append(newRecipe);
+        }
+    }
+    if(json.contains("resources") && json["resources"].isObject())
+    {
+        QJsonObject object = json["resources"].toObject();
+        foreach (QString name, object.keys())
+        {
+            QJsonObject resource = object[name].toObject();
+            FactorioResource newResource;
+            newResource.jsonRead(resource);
+            mResources.append(newResource);
+        }
+    }
+}
--- FactorioProductionAnalyzer/productioncalculator.h
+++ FactorioProductionAnalyzer/productioncalculator.h
@@ -1,7 +1,15 @@
 #ifndef PRODUCTIONCALCULATOR_H
 #define PRODUCTIONCALCULATOR_H
 
+#include <QJsonObject>
 #include <QObject>
+
+#include "factorioassembler.h"
+#include "factorioitem.h"
+#include "factoriominer.h"
+#include "factoriomodule.h"
+#include "factoriorecipe.h"
+#include "factorioresource.h"
 
 /*************************************************************************
  * Defines
@@ -20,8 +28,18 @@ public:
 
     void setFile(QString &fileName);
 
+    bool fileRead(void);
+    void jsonRead(const QJsonObject &json);
+
 private:
     QString mFileName;
+
+    QVector<FactorioAssembler>  mAssemblers;
+    QVector<FactorioItem>       mItems;
+    QVector<FactorioMiner>      mMiners;
+    QVector<FactorioModule>     mModules;
+    QVector<FactorioRecipe>     mRecipes;
+    QVector<FactorioResource>   mResources;
 };
 
 #endif // PRODUCTIONCALCULATOR_H
