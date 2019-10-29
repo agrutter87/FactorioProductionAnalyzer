f508559b2816ec8b6a7b94bf481b8f086a506e8e
agrutter87 <agrutter87@gmail.com>
2019-10-28 19:59:33 -0500

    Added comments to FactorioAssembler

--- FactorioProductionAnalyzer/factorioassembler.cpp
+++ FactorioProductionAnalyzer/factorioassembler.cpp
@@ -18,30 +18,42 @@ void FactorioAssembler::jsonRead(const QJsonObject &json)
 #if DEBUG_FACTORIO_ASSEMBLER
     qDebug() << __PRETTY_FUNCTION__;
 #endif
+    /* Read the allowed_effects value if it exists */
     if (json.contains("allowed_effects"))
     {
+        /* Read the allowed_effects to a separate JSON object */
         QJsonObject allowedEffects = json["allowed_effects"].toObject();
+
+        /* Read the consumption value if it exists */
         if(allowedEffects.contains("consumption") && allowedEffects["consumption"].isBool())
         {
             mAllowedEffectConsumption = allowedEffects["consumption"].toBool();
         }
+
+        /* Read the pollution value if it exists */
         if(allowedEffects.contains("pollution") && allowedEffects["pollution"].isBool())
         {
             mAllowedEffectPollution = allowedEffects["pollution"].toBool();
         }
+
+        /* Read the productivity value if it exists */
         if(allowedEffects.contains("productivity") && allowedEffects["productivity"].isBool())
         {
             mAllowedEffectProductivity = allowedEffects["productivity"].toBool();
         }
+
+        /* Read the speed value if it exists */
         if(allowedEffects.contains("speed") && allowedEffects["speed"].isBool())
         {
             mAllowedEffectSpeed = allowedEffects["speed"].toBool();
         }
     }
 
+    /* Read the crafting_categories value if it exists */
     if(json.contains("crafting_categories") && json["crafting_categories"].isArray())
     {
         QJsonArray array = json["crafting_categories"].toArray(array);
+
         /* Loop through the members of the JSON array */
         for(int index = 0; index < array.size(); ++index)
         {
@@ -49,23 +61,27 @@ void FactorioAssembler::jsonRead(const QJsonObject &json)
         }
     }
 
+    /* Read the crafting_speed value if it exists */
     if(json.contains("crafting_speed") && json["crafting_speed"].isDouble())
     {
         mCraftingSpeed = json["crafting_speed"].toDouble();
     }
+
+    /* Read the ingredient_count value if it exists */
     if(json.contains("ingredient_count") && json["ingredient_count"].isDouble())
     {
         mIngredientCount = json["ingredient_count"].toDouble();
     }
+
+    /* Read the module_inventory_size value if it exists */
     if(json.contains("module_inventory_size") && json["module_inventory_size"].isDouble())
     {
         mModuleInventorySize = json["module_inventory_size"].toDouble();
     }
 
-    /* Find "name" section in JSON object and ensure it is set to a String */
+    /* Read the name value if it exists */
     if (json.contains("name") && json["name"].isString())
     {
-        /* Set mName to value in JSON */
         mName = json["name"].toString();
     }
  }
